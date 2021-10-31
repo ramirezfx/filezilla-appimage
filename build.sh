cb8b3b72bdbfd677f9fcf7cb7ff9afac4f74b882
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2021-10-31 10:45:55 +0100

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+rm appimagetool-x86_64.AppImage
+rm FileZilla_3.55.1_x86_64-linux-gnu.tar
+rm -Rf FileZilla3.AppDir
+DLLINK=$(wget https://filezilla-project.org/download.php?platform=linux64 -q -O - | grep https://dl | cut -d '"' -f 4)
+wget -O filezilla-latest.tar.bz2 $DLLINK
+bunzip2 filezilla-latest.tar.bz2
+tar xf filezilla-latest.tar
+mv FileZilla3 FileZilla3.AppDir
+mkdir FileZilla3.AppDir/usr
+cd FileZilla3.AppDir
+mv bin usr
+mv lib usr
+mv share usr
+cd ..
+cp FileZilla3.AppDir/usr/share/applications/filezilla.desktop FileZilla3.AppDir
+cp FileZilla3.AppDir/usr/share/icons/hicolor/scalable/apps/filezilla.svg FileZilla3.AppDir
+chmod 700 AppRun
+cp AppRun FileZilla3.AppDir
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod 700 appimagetool-x86_64.AppImage
+./appimagetool-x86_64.AppImage FileZilla3.AppDir
---
+++ integrate-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IMGNAME=filezilla.png
+DESKTOPFILE=gimp-appimage.desktop
+
+APPPATH=$(kdialog --getopenfilename)
+
+echo "[Desktop Entry]" > $DESKTOPFILE
+echo "Version=1.0" >> $DESKTOPFILE
+echo "Name=FileZilla Pro" >> $DESKTOPFILE
+echo "GenericName=FTP client" >> $DESKTOPFILE
+echo "Comment=Download and upload files via FTP, FTPS and SFTP" >> $DESKTOPFILE
+echo "Exec=$APPPATH --appimage-extract-and-run" >> $DESKTOPFILE
+echo "Terminal=false" >> $DESKTOPFILE
+echo "Icon=filezilla.png" >> $DESKTOPFILE
+echo "Type=Application" >> $DESKTOPFILE
+echo "Categories=Network;FileTransfer;" >> $DESKTOPFILE
+echo "StartupWMClass=Filezilla" >> $DESKTOPFILE
+
+sudo mv $DESKTOPFILE /usr/share/applications
+sudo cp $IMGNAME /usr/share/icons
